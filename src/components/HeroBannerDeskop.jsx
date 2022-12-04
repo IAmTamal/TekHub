78d1a8bba75202c888b92e65bdf0750bdb03b543
Tamal Das <gyansujan69@gmail.com>
2022-12-04 23:12:18 +0530

    feat : Customized scrollbar

--- src/components/HeroBannerDeskop.jsx
+++ src/components/HeroBannerDeskop.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import { ReactComponent as HeyBannerMobile } from '../assets/heymobile.svg';
-import { ReactComponent as HeyBannerDesktop } from '../assets/heydesktop.svg';
-// import { ReactComponent as HeyBannerDesktop2 } from '../assets/heydesktop2.png';
-// import { ReactComponent as HeyBanner } from '../assets/heydesktop.png';
 import "../styles/HeroBannerDesktop.css"
 
 
@@ -21,7 +17,7 @@ const HeroBannerDeskop = () => {
                             <p className='heroleftdiv_p3'>To help you connect with other amazing developers, products, content and community </p>
                             <button className='heroleftdiv_button'>Get started now</button>
                         </div>
-                        {/* <HeyBannerDesktop2 className="heroright_img" /> */}
+
                         <img src="https://i.ibb.co/jWvwqsK/heydesktop2.png" alt="" />
                     </div>
                 </div>
